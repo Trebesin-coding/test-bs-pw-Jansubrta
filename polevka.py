a14802200fc856ae660b8a753181f5372c0614ef
Jansubrta <subrta121@trebesin.cz>
2025-02-12 08:56:46 +0100

    poslilani

--- polevka.py
+++ polevka.py
@@ -1,14 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 
 def main():
-
-
+    URL= ("https://js-trebesin.github.io/bsoup-exam/")
+    response= requests.get(URL)
+    soup=BeautifulSoup(response.content, "html.parser")
+    all_p = soup.find_all("p")
+    list_p = []
+    print (list_p)
     # response = requests.get(url)
 
-    # BeautifulSoup(response.content, "html.parser") <--- Úkol: popiš krátce, co tohle dělá
-
+    # BeautifulSoup(response.content, "html.parser") <--- Úkol: popiš krátce, co tohle dělá zkontroluje jestli jsme připojeni na stranku
+    for p in all_p:
+        list_p.append(p.text)
+        print(list_p)
+   
+    with open("data.json", mode="w" )as file:
+        json.dump(list_p, file ,indent = 4)
 
 if __name__ == "__main__":
     main()
--- secret.py
+++ secret.py
@@ -1,21 +1,28 @@
 from playwright.sync_api import sync_playwright
 import os
 
+Login = "Jarmil"
+Heslo ="Admin123"
 
 
 
 def main():
 
-    # with sync_playwright() as p:
+     with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page= browser.New_page
+        page.goto("https://js-trebesin.github.io/playwright-exam/")
 
-        # browser = p.chromium.launch(headless=False)
+        page.fill("INPUT[Login='answer']"Login)
+        page.fill("INPUT[Heslo='answer']"Heslo)
+        page.click("botton[type = 'submit']")
 
         # !!!
         # na page.locator(selector) se dá použít funkce .text_content(), která vypíše text daného prvku
         # !!!
 
 
-        # browser.close()
+        browser.close()
     
 
 if __name__ == "__main__":
